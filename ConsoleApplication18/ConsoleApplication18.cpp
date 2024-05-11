7e1d181927fc648a4f9f99929619d2a3b3698ebb
Роман <@DESKTOP-NKNVI54>
2024-05-11 11:33:20 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication18/ConsoleApplication18.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int op;
+	int a;
+	int b;
+	cin >> op;
+	cin >> a;
+	cin >> b;
+	switch (op)
+	{
+	case 1:
+		cout << a + b;
+	case 2:
+		cout << a - b;
+		break;
+	}
+}
